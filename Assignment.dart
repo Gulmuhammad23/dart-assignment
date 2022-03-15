24325301225eac8ca3936461fefe7e67c6dbb928
Gulmuhammad23 <100403996+Gulmuhammad23@users.noreply.github.com>
2022-03-15 14:40:36 +0500

    Add files via upload

---
+++ Assignment.dart
@@ -0,0 +1,98 @@
+import 'dart:io';
+
+void main() {
+  // Task 1
+  // var pdname;
+  // var tprice;
+  // stdout.write("Enter your name.");
+  // stdin.readLineSync();
+  // stdout.write("Enter your product.");
+  // pdname = stdin.readLineSync();
+  // print("Your ${pdname} price is 300.");
+  // var Price = 300;
+  // stdout.write("Enter quantity.");
+  // var Qntty = int.parse(stdin.readLineSync()!);
+  // tprice = (Qntty * Price);
+  // print("You purchased ${Qntty} ${pdname}");
+  // print("Your total payment is ${tprice}.");
+
+//  Task 2
+  // stdout.write("Enter First Number.");
+  // var a = int.parse(stdin.readLineSync()!);
+  // stdout.write("Enter Scound Number.");
+  // var b = int.parse(stdin.readLineSync()!);
+  // int c = a + b;
+  // print("The sum of ${a} and ${b} is ${c}");
+// Task 3
+//   stdout.write("Enter First Number.");
+//   var a = int.parse(stdin.readLineSync()!);
+//   stdout.write("Enter Scound Number.");
+//   var b = int.parse(stdin.readLineSync()!);
+//   // plus Two Number
+//   int c = a + b;
+//   print("The plus of ${(a)} and ${(b)} is ${(c)}");
+//   // minus two numbers
+//   c = a - b;
+//   print("The minus of ${(a)} and ${(b)} is ${(c)}");
+//   // multiply two numbers
+//   c = a * b;
+//   print("The multiply of ${(a)} and ${(b)} is ${(c)}");
+// // division two numbers
+//   double d = (a / b);
+//   print("The division of ${(a)} and ${(b)} is ${(d)}");
+// // Task 4
+  // print("1. Ticket price is 600.");
+  // stdout.write("How many tickets do you want? ");
+  // var a = int.parse(stdin.readLineSync()!);
+  // int Ticketprice = 600;
+  // int total_price = a * Ticketprice;
+  // print("you purchased ${a} tickets your total payment is ${total_price}");
+// Task 5
+  // stdout.write("Enter temp in celsius ");
+  // var c = double.parse(stdin.readLineSync()!);
+  // double F = (c * 9 / 5) + 32;
+  // print("${c} Celsius is equal to ${F} Fahrenhiet .");
+
+  //Task 6
+  // stdout.write("What do you want in Food.");
+  // var iteam1 = stdin.readLineSync();
+  // stdout.write("How much.");
+  // var Quantity = int.parse(stdin.readLineSync()!);
+  // stdout.write("Your like somthing in spicy.");
+  // var iteam2 = stdin.readLineSync();
+  // stdout.write("How much.");
+  // var Qntty = int.parse(stdin.readLineSync()!);
+  // double Iteam1_price = Quantity * 700;
+  // double Iteam2_price = Qntty * 1500;
+  // double Total_price = Iteam1_price + Iteam2_price;
+  // print(
+  //     "Total price of ${iteam1} is ${Iteam1_price}Rs\nQuantity of ${iteam1} is ${Quantity} \nPrice of spicy food is ${Iteam2_price} Rs\nQuantity of spicy food ${Qntty} \nTotal Price of Both Items is ${Total_price} Rs");
+
+  // Task7
+  // stdout.write("Enter total marks of IT subject ");
+  // var total_marks = int.parse(stdin.readLineSync()!);
+  // stdout.write("Enter marks you secured in IT . ");
+  // var marks = int.parse(stdin.readLineSync()!);
+  // double Percentage = (marks / total_marks) * 100;
+  // print("your percentage is ${Percentage}%");
+
+  // Task8
+  // stdout.write("Enter Dollars.");
+  // var A = int.parse(stdin.readLineSync()!);
+  // stdout.write("Enter Riyal.");
+  // var B = int.parse(stdin.readLineSync()!);
+  // double AtoPKR = A * 178.70;
+  // double BtoPKR = B * 47.63;
+  // double Total_Amount = AtoPKR + BtoPKR;
+  // print(
+  //     "${A} Dollars are ${AtoPKR} to pakistani Rupess.178.70\n${B} Saudi Riyals are ${BtoPKR} To pakistani Rupess.47.63\nThe total of Dollar and SR in PKR is ${Total_Amount}Rs");
+
+  // Task 9
+  // stdout.write("Enter Hours of your job you work here.");
+  // var hours = double.parse(stdin.readLineSync()!);
+  // stdout.write("Enter rate per hour.");
+  // var rate_perhour = double.parse(stdin.readLineSync()!);
+  // double pay = hours * rate_perhour;
+  // print(
+  //     "You work ${hours} hours. \nYour rate of per hour is ${rate_perhour}Rs \nYour total income is ${pay}Rs");
+}
